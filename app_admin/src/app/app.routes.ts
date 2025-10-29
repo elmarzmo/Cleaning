31383453e4d71a0151eb0bf5bfe389a2c47c27e1
Mohamed Elmarzougui <Mohamed1elmarzougui@gmail.com>
2025-10-29 01:05:58 -0500

    fix the quote service!

--- app_admin/src/app/app.routes.ts
+++ app_admin/src/app/app.routes.ts
@@ -1,3 +1,11 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Quote } from './quote/quote';
 
-export const routes: Routes = [];
+
+ export const routes: Routes = [
+    { path : '', component: Home}, //deafault route
+    { path: 'quote', component: Quote} // Quote page
+ ];
+
+ 
