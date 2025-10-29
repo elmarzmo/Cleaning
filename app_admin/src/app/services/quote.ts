7d56296c14f9a0ed96be847ce2341331d77dedfd
Mohamed Elmarzougui <Mohamed1elmarzougui@gmail.com>
2025-10-29 01:09:13 -0500

    DDDA

--- app_admin/src/app/services/quote.ts
+++ app_admin/src/app/services/quote.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class Quote {
+  // backend
+  private apiUrl = 'http://localhost:5000/api/quotes';
+
+  constructor(private hhtp:HttpClient){}
+
+  sendQuote(data: any): Observable<any> {
+    return this.http.post(this.apiUrl, data);
+  }
+
+  getQuotes(): Observable<any> {
+    return this.http.get(this.apiUrl);
+  }
   
 }
