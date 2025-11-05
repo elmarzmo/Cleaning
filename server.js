8964ac9bc8f9bc49c87727beb5e5b1fc67bbd888
Mohamed Elmarzougui <mohamed1elmarzougui@gmail.com>
2025-11-05 12:45:35 -0600

    Quote Page CSS

--- server.js
+++ server.js
@@ -16,20 +16,11 @@ app.use(express.json());
 app.use('/api/service', serviceRoutes);
 
 //  Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then (()=>{
-    console.log(' Connected to MongoDB');
-
-})
-.catch((error)=>{
-    console.error('MongoDB connection error: ', error);
-});
+mongoose.connect(process.env.MONGO_URI)
+.then (()=> console.log(' Connected to MongoDB'))
+.catch((error)=> console.error('MongoDB connection error: ', error));
 
 // Start the server 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+    console.log(`Server running on port ${PORT}`);});
