ba6f38633fbd6861793a46389c4766acb7a021e7
Mohamed Elmarzougui <mohamed1elmarzougui@gmail.com>
2025-11-15 00:17:03 -0600

    remove Angular and create the handlebars pages

--- app_admin/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { RouterModule, Routes } from '@angular/router';
-import { Home } from './home/home';
-import { Quote } from './quote/quote';
-
-
- export const routes: Routes = [
-    { path : '', component: Home}, //deafault route
-    { path: 'quote', component: Quote} // Quote page
- ];
