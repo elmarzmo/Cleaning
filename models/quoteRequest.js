88d91617d96bb23159c8e18574addf93d66e1408
Mohamed Elmarzougui <mohamed1elmarzougui@gmail.com>
2025-11-27 19:13:23 -0600

    create the quote model Mongoose

--- models/quoteRequest.js
+++ models/quoteRequest.js
@@ -2,14 +2,34 @@ const mongoose = require('mongoose');
 
 
 const quoteSchema= new mongoose.Schema({
-    cutomerName: {
+    name: {
         type: String,
         required: true
     },
-    address: String,
-    serviceRequested: { type: String, 
-        required: true},
-        additionalNotes: String,
-        estimatedPrice: Number, // for later!!
-});
+   email: {
+        type: String,
+        required: true
+    },
+   phone: {
+        type: String,
+        required: true
+    },
+    zip: {
+        type: String,
+        required: true
+    },
+    service: {
+        type: String,
+        required: true
+    },
+    frequency: {
+        type: String,
+        required: true
+    },
+    note: {
+        type: String
+    },
+    
+},{ timestamps: true});
 
+module.exports = mongoose.model('QuoteRequest', quoteSchema);
