359e8607dce4b9be440a9cddf6dd18d755a19df2
Mohamed Elmarzougui <mohamed1elmarzougui@gmail.com>
2025-11-28 21:46:09 -0600

    Create quote controller

---
+++ controllers/quoteComtroller.js
@@ -0,0 +1,59 @@
+const QuoteRequest = require('../models/quoteRequest');
+
+
+// Get all quote requests
+exports.getAllQuotes = async (req, res) => {
+    try {
+        const quotes = await QuoteRequest.find();
+        res.status(200).json(quotes);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server error' });
+    }
+};
+
+// submitted quote request
+exports.submitQuote = async (req, res) => {
+    try {
+        const newQuote = new QuoteRequest({
+            name: req.body.name,
+            email: req.body.email,
+            phone: req.body.phone,
+            zip: req.body.zip,
+            service: req.body.service,
+            frequency: req.body.frequency,
+            notes: req.body.notes
+        });
+        await newQuote.save();
+        res.status(200).json({ message: 'Quote request submitted successfully' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server error' });
+    }
+};
+
+// Get a single quote request by ID
+exports.getQuoteById = async (req, res) => {
+    try {
+        const quote = await QuoteRequest.findById(req.params.id);
+        if (!quote) return res.status(404).json({ message: 'Quote request not found' });
+        res.status(200).json(quote);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server error' });
+    }
+};
+
+// 
+
+// Delete a quote request by ID
+exports.deleteQuoteById = async (req, res) => {
+    try {   
+        const deleted = await QuoteRequest.findByIdAndDelete(req.params.id);
+        if (!deleted) return res.status(404).json({ message: 'Quote request not found' });
+        res.status(200).json({ message: 'Quote request deleted successfully' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server error' });
+    }
+};
--- server.js
+++ server.js
@@ -50,28 +50,7 @@ app.get('/services', (req, res) => {
 
 app.use('/api/quotes', quoteRequests);
 
-/*// Endpoint to handle quote form submission 
 
-app.post('/submit-quote', async (req, res) => {
-    try {
-    const newQuote = new QuoteRequest({
-        name: req.body.name,
-        email: req.body.email,
-        phone: req.body.phone,
-        zip: req.body.zip,
-        service: req.body.service,
-        frequency: req.body.frequency,
-        notes: req.body.notes
-    });
-    await newQuote.save();
-   
-    console.log('Quote request received: ',{name: req.body.name });
-    res.status(200).redirect('/quote-success');   
-    } catch (error) {
-        console.error('Error processing quote request: ', error);
-    res.status(500).json({message: 'Error processing quote request'});
-    }
-});*/
 // Use service routes 
 app.use('/api/service', serviceRoutes);
 
