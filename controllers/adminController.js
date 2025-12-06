20337940049ee96f85c12326e997875500522406
Mohamed Elmarzougui <mohamed1elmarzougui@gmail.com>
2025-12-06 02:28:11 -0600

    require login after closing the tab

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -38,8 +38,8 @@ exports.login = async (req, res) => {
 
         res.cookie('token', token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            maxAge: 24 * 60 * 60 * 1000
+            secure: process.env.NODE_ENV === 'production'
+           // maxAge: 24 * 60 * 60 * 1000
         });
 
           res.status(200).json({ message: 'Login successful' });
