fd8da4856a90e98ab64e943aef80e2752e5caa72
Mohamed Elmarzougui <mohamed1elmarzougui@gmail.com>
2025-12-13 00:51:19 -0600

    change the contact route so it can use contactController

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,13 +1,12 @@
-const express = require('express');
 const router = express.Router();
-const adminController = require('../controllers/admin/adminController');
 const verifyToken = require('../middleware/authMiddleware');
+const contactController = require('../controllers/contactController');
 
 
 // Endpoint to handle contact form submission
-router.get('/', adminController.getContacts);
+router.get('/', contactController.getAdminContacts);
 
-router.post('/update', verifyToken, adminController.updateContacts);
+router.post('/update', verifyToken, contactController.updateAdminContacts);
 
 
 module.exports = router;
