5aba68d78ef62fbb64e2690523d7e14d68b4139f
Mohamed Elmarzougui <mohamed1elmarzougui@gmail.com>
2025-12-21 02:18:26 -0600

    phone screeen css fixed

--- server.js
+++ server.js
@@ -111,6 +111,6 @@ mongoose.connect(process.env.MONGO_URI)
 .catch((error)=> console.error('MongoDB connection error: ', error));
 
 // Start the server 
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);});
